0808cce9d507dadb74d7dd4ff9791ffde86edb3a
Sushil Kumar <sushilkumar168141@gmail.com>
2019-06-07 19:45:09 +0530

    Java syntax and comments

---
+++ HelloWorld.java
@@ -0,0 +1,18 @@
+/* Getting started with java  
+In java, every application must begins with a class name, and that class must match the file name.
+Everyline of  code that runs in java must be inside of a class
+A class should always starts with an uppercase first letter.
+Java is case-sensitive.
+main() : The main() method is requiredin every program. Code execution will start from main method.
+In java each code statement must end with a semicolon.
+
+
+These are also example of multi-line comments.
+*/
+public class HelloWorld	// class declaration with the name "HelloWorld" 
+{
+	public static void main(String[] args) 		// This is an example of a single line comment.
+	{
+		System.out.println("Hello World");		//To print output to the screen & every code statement must end with a ;
+	}
+}
